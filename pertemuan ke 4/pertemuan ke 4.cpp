1820425a830571dcf9f604046e233d286af8decf
firyalshafa <shafafiryal782@gmail.com>
2024-03-14 14:48:50 +0700

    memperbaiki step 4a

--- pertemuan ke 4/pertemuan ke 4.cpp
+++ pertemuan ke 4/pertemuan ke 4.cpp
@@ -10,11 +10,12 @@ void insertionsort() {
 
     for (i = 1; i <= n - 1; i++) { // step 1 
         temp = arr[i]; // step 2
+
         j = i - 1; // step 3 
 
         while (j >= 0 && arr[j] >= temp) // step 4
         {
-            arr[j] = arr[j + 1]; // step 4a
+            arr[j + 1] = arr[j]; // step 4a
             j = j - 1; // step 4b 
 
         }
