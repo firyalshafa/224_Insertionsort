6d5a047eba63550735d6416a07ea217457b0c3aa
firyalshafa <shafafiryal782@gmail.com>
2024-03-14 14:57:55 +0700

    menambahkan input

--- pertemuan ke 4/pertemuan ke 4.cpp
+++ pertemuan ke 4/pertemuan ke 4.cpp
@@ -4,6 +4,35 @@
 #include <iostream>
 using namespace std;
 
+int n;
+int arr[20];
+
+
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+
+        }
+
+    }
+    cout << endl;
+    cout << "=============" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "==============" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << " : ";
+        cin >> arr[i];
+
+    }
+}
+
+
 void insertionsort() {
     int i, j, n, temp;
     int arr[20];
