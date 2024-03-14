89adabe9693874bcbed4af22fd704f4d1db050c6
firyalshafa <shafafiryal782@gmail.com>
2024-03-14 14:41:51 +0700

    membuat step 4a&4b

--- pertemuan ke 4/pertemuan ke 4.cpp
+++ pertemuan ke 4/pertemuan ke 4.cpp
@@ -10,6 +10,13 @@ void insertionsort() {
 
     for (i = 1; i <= n - 1; i++) { // step 1 
         temp = arr[i]; // step 2
+
+        while (j >= 0 && arr[j] >= temp) // step 4
+        {
+            arr[j] = arr[j + 1]; // step 4a
+            j = j - 1; // step 4b 
+
+        }
     }
 }
 
